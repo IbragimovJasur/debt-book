6605476b0bc31816511109810d74ac1a959f5880
00011581 <wiut00011581@gmail.com>
2021-10-12 22:32:21 +0500

    project is created, conf is implemented, .gitignore file is added

---
+++ conf/settings/local.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+SECRET_KEY = 'django-insecure-@uqq$5a_)*q2u&87+rna8omq%#s)(qn$=1x)u0l!9s*fbwcc9w'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_URL = '/static/'
+
+from conf.settings.base import *
