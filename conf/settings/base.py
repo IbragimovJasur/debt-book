21a6809c9a2d3b72aeb4afef93a2ef3efc97ffc4
IbragimovJasur <ij.ilkhambekovich@gmail.com>
2021-10-15 21:55:18 +0500

    changes in conf and in fixtures file of contacts app

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -37,7 +37,7 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
 }
 
