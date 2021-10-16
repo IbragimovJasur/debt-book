048ebf3d913f16d4aa01d7701f5b9c8360134385
IbragimovJasur <ij.ilkhambekovich@gmail.com>
2021-10-16 18:47:36 +0500

    changes in contacts.models file

--- apps/contacts/admin.py
+++ apps/contacts/admin.py
@@ -3,5 +3,5 @@ from .models import Currency, Contact
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ['owner', 'username']
+    list_display = ['owner', 'name', 'phone']
     list_display_links = ['owner']
--- apps/contacts/models.py
+++ apps/contacts/models.py
@@ -1,47 +1,24 @@
 from django.db import models
 from django.conf import settings
 
-class Currency(models.Model):
-    name = models.CharField(
-        "Sign of currency", max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
 class Contact(models.Model):
     class Meta:
         db_table = "contacts"
 
-    DEBT_TYPE_CHOICES = [
-        ("lent", "Lend"),
-        ("borrowed", "Borrow"),
-        ("paid", "Paid"),
-    ]
     owner = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name= 'contacts', null=True, blank=True)
-    debt_currency = models.ForeignKey(
-        Currency, on_delete=models.SET_NULL, related_name='contacts', null=True, blank= True)
-    username = models.CharField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='contacts', null=True, blank=True)
+    name = models.CharField(
         "Username", max_length=150)
+    avatar = models.ImageField(
+        "Profile photo", upload_to='users/avatar/', default='users/avatar/default.jpg')
     phone = models.CharField(
         "Phone number", max_length=50)
-    address = models.CharField(
-        "Address", max_length=250, null=True, blank=True)
-    comment = models.TextField(
-        "Any comment about contact", null=True, blank=True)
     card_number = models.CharField(
         "Card number", max_length=100, null=True, blank=True)
-    debt_type = models.CharField(
-        "Type of debt", choices=DEBT_TYPE_CHOICES, max_length=50)
-    debt_started_on = models.DateTimeField(
-        "Date when debt was given/taken")
-    debt_end_on = models.DateTimeField(
-        "Date when debt should be returned")
-    debt_amount = models.IntegerField(
-        "Debt amount")
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
-
+    created_on = models.DateTimeField(
+        "When contact is created", auto_now_add=True)
+    updated_on = models.DateTimeField(
+        "When contact is updated", auto_now=True)
+   
     def __str__(self):
         return self.owner.username
